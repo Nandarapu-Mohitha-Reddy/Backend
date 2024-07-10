02ab4c920a4124f32c6d63ff4f322df3d5637837
Nandarapu-Mohitha-Reddy <mohithareddynandarapu@gmail.com>
2024-07-10 06:17:41 +0530

    Project Submission

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-//index.js
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dbURI = process.env.MONGODB_URI;
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use("/", routes);
+
+// Connect to MongoDB
 mongoose
-  .connect("Your MongoDB URI", {
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => {
+    console.log("MongoDB connected successfully");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use("/", routes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    // Start Express server
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
